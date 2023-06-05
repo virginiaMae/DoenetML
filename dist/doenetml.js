64b07f08a1ba8ac687d7a3296f1eb48959d47104
Duane Nykamp <nykamp@umn.edu>
2023-06-05 14:53:46 -0500

    add dist folder

---
+++ dist/doenetml.js
@@ -0,0 +1,20 @@
+import { i as s, a3 as o, S as n, X as m, U as d, e as l, Y as p, T as c, Z as C, W as v, a2 as A, $ as f, a0 as u, a1 as x } from "./index-1fe56d63.js";
+import "react";
+import "react-dom";
+import "styled-components";
+export {
+  s as CodeMirror,
+  o as DarkmodeController,
+  n as DoenetML,
+  m as calculateOrderAndVariants,
+  d as cidFromText,
+  l as darkModeAtom,
+  p as determineNumberOfActivityVariants,
+  c as mathjaxConfig,
+  C as parseActivityDefinition,
+  v as retrieveTextFileForCid,
+  A as returnAllPossibleVariants,
+  f as returnNumberOfActivityVariantsForCid,
+  u as serializedComponentsReplacer,
+  x as serializedComponentsReviver
+};
